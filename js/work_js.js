f686124d0b21f1427f74fedf090776e8d86bb789
dnjwm8612 <dnjwm8612@gmail.com>
2021-12-10 17:55:08 +0900

    style Apply JavaScript to the menu button part, change CSS

--- js/work_js.js
+++ js/work_js.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
     $('.slider').bxSlider();
     
-    $("path").click(function(){
+    $("#close_icon").click(function(){
         $("#banner").addClass("active");
         $("section").css("margin-top", "0");
-    })
+    });
+    
+    $("#menubtn").click(function(){
+        var arrow_class_name = $("#button-icon").attr("class");
+        
+        var left_class = "svg-inline--fa fa-arrow-left fa-w-14";
+        
+        var right_class = "svg-inline--fa fa-arrow-right fa-w-14";
+        
+        if(arrow_class_name == left_class){
+            $("#button-icon").attr("class", right_class);
+            $("aside").css("display", "inline-flex");
+            $("#menubtn").removeClass("active");
+            $(".submenu").fadeToggle(300);
+            
+        }else{
+            $("#button-icon").attr("class", left_class);
+            $("aside").css("display", "");
+            $("#menubtn").addClass("active");
+            $(".submenu").hide();
+        }
+    });
     
 });
